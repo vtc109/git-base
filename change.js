0efedcfc37233b4292d5649536cc89a586cda296
congvt109 <congvubbk109@gmail.com>
2023-06-23 16:55:25 +0700

    15 change commit msg

--- change.js
+++ change.js
@@ -2,3 +2,5 @@ console.log("Xin chào");
 console.log("file này bị thiếu khi commit ");
 console.log("file này bị thiếu khi commit ");
 console.log("add thiếu file, commit khi có lôi ");
+// changing missing files
+// write a code calculate sum of a and b 
