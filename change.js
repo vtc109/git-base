b757602765968834d411077a063417e7813dc460
congvt109 <congvubbk109@gmail.com>
2023-06-26 15:44:31 +0700

    19-add file to main branch

--- change.js
+++ change.js
@@ -4,3 +4,4 @@ console.log("file này bị thiếu khi commit ");
 console.log("add thiếu file, commit khi có lôi ");
 // changing missing files
 // write a code calculate sum of a and b 
+console.log("file in the main branch, prepare to commit ");
